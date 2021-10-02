3fd07f6b1f2c515869907248c06bfafa2679e8b6
nemadep <nemade.p@northeastern.edu>
2021-10-01 22:01:13 -0700

    assignment 02

---
+++ Assignment02/src/classes/User.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author prathameshnemade
+ */
+public class User {
+    public int uniqueIdentifier;
+    public String type;
+    public String name;
+    public String email;
+    public String password;
+
+    /**
+     * @return the uniqueIdentifier
+     */
+    public int getUniqueIdentifier() {
+        return uniqueIdentifier;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+}
